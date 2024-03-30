320bdb4faa50b9b9e9a199e1d39c67422682798b
Sandipan80 <bhullarsandipan@gmail.com>
2024-03-30 14:00:20 +0530

    ..added Home, Login and register page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import "../styles/Home.css";
 
+import Button from "../components/Button";
+import Hero from "../components/Hero";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+    <div className="home flex flex-col justify-center pt-64">
+      <h1 className="text-teal-400 font-bold text-6xl text-center">
+        Freelance Marketplace for Students
+      </h1>
+      <div className="flex justify-center mt-10">
+        <Button>Explore</Button>
+      </div>
+      </div>
+      <Hero/>
+      </>
+  );
+};
 
-export default Home
+export default Home;
