12ee7ea8e68f71b01acdf3938528fffec04a0c7d
Sandipan80 <bhullarsandipan@gmail.com>
2024-03-30 18:54:53 +0530

    ..updated home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import HeroCard from "./HeroCard";
 import { PiStarFourDuotone } from "react-icons/pi";
+import {Accordion, AccordionItem} from "@nextui-org/react";
+const defaultContent =
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 const Hero = () => {
+
   return (
-    <div className="mt-[70vh]">
+    <div className="mt-48">
       <div className="text-5xl flex gap-2 text-center justify-center">
-        <PiStarFourDuotone className="text-teal-400" />
-        <h1 className=" text-white font-semibold ">
+        <PiStarFourDuotone className="text-yellow-400" />
+        <h1 className=" text-black font-semibold tracking-wide">
           Best place for students to get their first freelance work
         </h1>
       </div>{" "}
@@ -15,8 +19,21 @@ const Hero = () => {
         <HeroCard />
         <HeroCard />
       </div>
+      <div className="mx-48 mt-20 mb-20 borderhh-2 shadow-lg rounded-md bg-blue-50">
+      <Accordion variant="light">
+      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+        {defaultContent}
+      </AccordionItem>
+    </Accordion>
+      </div>
     </div>
   );
-};
+  }    
 
 export default Hero;
